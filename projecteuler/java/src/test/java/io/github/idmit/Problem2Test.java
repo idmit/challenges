5b3107d2293da8dd6e4ecdb511e55172184570eb
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-07-19 18:48:53 +0300

    Add the Project Euler Problem 2

--- projecteuler/java/src/test/java/io/github/idmit/Problem2Test.java
+++ projecteuler/java/src/test/java/io/github/idmit/Problem2Test.java
@@ -1,4 +1,12 @@
 package io.github.idmit;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class Problem2Test {
+    @Test
+    public void test1() {
+        Assert.assertEquals(4_613_732, Problem2.solveFor(4_000_000));
+    }
 }
